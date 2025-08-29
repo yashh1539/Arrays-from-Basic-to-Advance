1751f5b2ec9bbbf75a2cf1232fb3b9855aae815f
Yash Kshirsagar <yash.kshirsagar001@gmail.com>
2025-08-29 09:26:51 +0530

    Bubblesort.java  (in assending order)
    in assending order

--- Bubblesort.java
+++ Bubblesort.java
@@ -7,46 +7,41 @@ public class Bubblesort {
 
         Scanner scan = new Scanner(System.in);
         System.out.println("ENTER LENGTH OF ARRAY");
-        int n= scan.nextInt();
-        System.out.println("enter "+ n+ " elements");
+        int n = scan.nextInt();
+        System.out.println("enter " + n + " elements");
 
-        int elements[]= new int[n];
-        for (int i = 0; i <n ; i++) {
-            elements[i]= scan.nextInt();
+        int elements[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            elements[i] = scan.nextInt();
         }
         Bubblesortalgo(elements);
         display(elements);
     }
-    public static void Bubblesortalgo(int elements[]){
-        int flag;
-        int n = elements.length;
-        for (int i = 0; i < n-1; i++) {
-             flag=0;
-            for (int j = 0; j <n-1-i ; j++) {
-                if (elements[j]>elements[j+1]){
-                    int temp=elements[j];
-                    elements[j]=elements[j+1];
-                    elements[j+1]=temp;
 
-                    flag=1;
+    public static void Bubblesortalgo(int elements[]) {
+        int n = elements.length;
+        for (int i = 0; i < n - 1; i++) {
+            int flag = 0;  // reset each pass
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (elements[j] > elements[j + 1]) {
+                    int temp = elements[j];
+                    elements[j] = elements[j + 1];
+                    elements[j + 1] = temp;
+                    flag = 1;
                 }
             }
-            if (flag==0){
-                System.out.println("already sorted");
+            if (flag == 0) {
+                System.out.println("Already sorted");
                 break;
             }
-
-
         }
-
     }
 
-    public static void display(int elements[]){
+    public static void display(int elements[]) {
         int n = elements.length;
         System.out.println("Sorted elements are ");
-        for (int i = 0; i <n ; i++) {
-            System.out.print(elements[i]+" ");
-
+        for (int i = 0; i < n; i++) {
+            System.out.print(elements[i] + " ");
         }
         System.out.println();
     }
